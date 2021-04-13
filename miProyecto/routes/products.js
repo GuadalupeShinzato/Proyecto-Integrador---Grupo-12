4921490a9095fbe5b89ba09a60dc23a334c518ec
gocampos1 <63106322+gocampos1@users.noreply.github.com>
2021-04-12 22:21:06 -0400

    commit 2 go

---
+++ miProyecto/routes/products.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var router = express.Router();
+const controller = require('../Controllers12/productController')
+
+// * GET home page. * /
+
+router.get('/', controller.index);
+router.get('/add', controller.add)
+
+module.exports = router 
