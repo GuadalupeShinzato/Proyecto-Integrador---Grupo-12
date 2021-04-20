95377bdafd6c4df1e33ac2b70a2cbec2f4dcee09
gocampos1 <63106322+gocampos1@users.noreply.github.com>
2021-04-20 20:46:13 -0300

    data.jsss

--- miProyecto/productos/data.js
+++ miProyecto/productos/data.js
@@ -6,26 +6,25 @@ let libros = {
            titulo: "The Dictionary of Lost Stars",
            autor: "Pip Williams",
            portada: "/imgenes/Hardcode/1/foto1.jpeg",
-           descripcion: "Bow Before Your Queen Or Bleed Before Her...",
+           descripcion: "Bow Before Your Queen Or Bleed Before Her..."
 
-           "From #1 New York Times bestselling author Jennifer L. Armentrout comes book three in her Blood and Ash series.",
-           
-           "She's been the victim and the survivor...",
+           "From #1 New York Times bestselling author Jennifer L. Armentrout comes book three in her Blood and Ash series."
+           "She's been the victim and the survivor..."
            
            "Poppy never dreamed she would find the love she's found with Prince Casteel. She wants to revel in her happiness but first they must free his brother and find hers. It's a dangerous mission and one with far-reaching consequences neither dreamed of. Because Poppy is the Chosen, the Blessed. The true ruler of Atlantia. She carries the blood of the King of Gods within her. By right the crown and the kingdom are hers.",
-           
+
         }, 
         {
             titulo: "Go Tell the Bees That I am Gone",
             autor: "Diana Gabaldon",
             portada:"/imgenes/Hardcode/2/f2.1.jpeg",
-            descripcion: "The author of the #1 New York Times bestselling Outlander series returns with the newest novel in the epic tale.",
+            descripcion: "The author of the #1 New York Times bestselling Outlander series returns with the newest novel in the epic tale."
             
-            "The past may seem the safest place to be . . . but it is the most dangerous time to be alive. . . .",
+            "The past may seem the safest place to be . . . but it is the most dangerous time to be alive. . . ."
             
-            "Jamie Fraser and Claire Randall were torn apart by the Jacobite Rising in 1743, and it took them twenty years to find each other again.",
+            "Jamie Fraser and Claire Randall were torn apart by the Jacobite Rising in 1743, and it took them twenty years to find each other again."
             
-            "Now the American Revolution threatens to do the same." ,
+            "Now the American Revolution threatens to do the same." 
             
             "It is 1779 and Claire and Jamie are at last reunited with their daughter, Brianna, her husband, Roger, and their children on Fraser’s Ridge. Having the family together is a dream the Frasers had thought impossible."
          },
@@ -33,7 +32,7 @@ let libros = {
             titulo: "Once Upon a Broken Heart",
             autor: "Stephanie Garber",
             portada: "/imgenes/Hardcode/3/f3.1.jpeg",
-            descripcion: "Once Upon a Broken Heart marks the launch of a new series about love, curses, and the lengths that people will go to for happily ever after from Stephanie Garber, the #1 New York Times bestselling author of Caraval",
+            descripcion: "Once Upon a Broken Heart marks the launch of a new series about love, curses, and the lengths that people will go to for happily ever after from Stephanie Garber, the #1 New York Times bestselling author of Caraval"
 
             "Evangeline Fox was raised in her beloved father’s curiosity shop, where she grew up on legends about immortals, like the tragic Prince of Hearts. She knows his powers are mythic, his kiss is worth dying for, and that bargains with him rarely end well."
          },
@@ -44,9 +43,9 @@ let libros = {
             portada: "/imgenes/Hardcode/4/4.1.jpeg",
             descripcion: "Bow Before Your Queen Or Bleed Before Her...",
 
-            "From #1 New York Times bestselling author Jennifer L. Armentrout comes book three in her Blood and Ash series.",
-            
-            "She's been the victim and the survivor...",
+            "From #1 New York Times bestselling author Jennifer L. Armentrout comes book three in her Blood and Ash series."
+         
+            "She's been the victim and the survivor..."
             
             "Poppy never dreamed she would find the love she's found with Prince Casteel. She wants to revel in her happiness but first they must free his brother and find hers. It's a dangerous mission and one with far-reaching consequences neither dreamed of. Because Poppy is the Chosen, the Blessed. The true ruler of Atlantia. She carries the blood of the King of Gods within her. By right the crown and the kingdom are hers."
          }, 
