8e93f191e0c66a69d96f80897f2a46ee4c9ed358
jelizalde21 <63111904+jelizalde21@users.noreply.github.com>
2021-04-20 20:35:59 -0300

    datoss

--- miProyecto/productos/data.js
+++ miProyecto/productos/data.js
@@ -1,29 +1,5 @@
 let libros = {
     lista: [
-        {
-           titulo:
-           autor:
-           portada:
-           descripcion: 
-        }, 
-        {
-            titulo:
-            autor:
-            portada:
-            descripcion: 
-         },
-         {
-            titulo:
-            autor:
-            portada:
-            descripcion: 
-         },
-         {
-            titulo:
-            autor:
-            portada:
-            descripcion: 
-         }, 
          {
             titulo:
             autor:
@@ -54,6 +30,30 @@ let libros = {
             portada:
             descripcion: 
          },
+         {
+            titulo: "Atack on Titan",
+            autor: "Hajime Isayama",
+            portada: "/images/Hardcode/6/6.jpeg",
+            descripcion: "Once hailed as humanity’s greatest hope, Eren has since become unrecognizable. With the Attack Titan now morphed into a grotesque, colossal skeleton of its former self, Eren continues to control the wave of Titans laying waste to the world, extinguishing countless homes, towns, and lives in their wake. In order to salvage what’s left of humanity, Mikasa, Armin, and the Survey Corps decide to confront Eren head-on. But to stop the Rumbling, they’ll have to dedicate much more than their hearts..."
+         }, 
+         {
+             titulo: "The christmas pig",
+             autor: "J.K. Rowling",
+             portada: "/images/Hardcode/5/5.1.jpeg",
+             descripcion: "Jack loves his childhood toy, Dur Pig. DP has always been there for him, through good and bad. Until one Christmas Eve something terrible happens – DP is lost. But Christmas Eve is a night for miracles and lost causes, a night when all things can come to life… even toys. And Jack’s newest toy – the Christmas Pig (DP’s replacement) – has a daring plan: Together they’ll embark on a magical journey to seek something lost, and to save the best friend Jack has ever known…"
+          },
+          {
+             titulo: "Freed",
+             autor: "El James",
+             portada: "/images/Hardcode/7/7.1.jpeg",
+             descripcion: "You are cordially invited to the wedding of the decade, when Christian Grey will make Anastasia Steele his wife. But is he really husband material? His dad is unsure, his brother wants to organize one helluva bachelor party, and his fiancée won't vow to obey...",
+          },
+          {
+             titulo: "The Good Sister",
+             autor: "Sally Hepworth",
+             portada: "/images/Hardcode/8/8.1.jpeg",
+             descripcion: "Fern Castle works in her local library. She has dinner with her twin sister Rose three nights a week. And she avoids crowds, bright lights and loud noises as much as possible. Fern has a carefully structured life and disrupting her routine can be...dangerous."
+          }, 
     ],
 };
 
