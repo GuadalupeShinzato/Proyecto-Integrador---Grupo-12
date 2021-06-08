2925fb2bb170a0bebf0f1e3add148380d12eed8e
Guadalupe Shinzato <gshinzato@udesa.edu.ar>
2021-06-07 23:44:32 -0300

    search

--- miProyecto/Controllers12/searchController.js
+++ miProyecto/Controllers12/searchController.js
@@ -1,4 +1,15 @@
+const db = require('../Database/models');
+const Op = db.Sequelize.Op;
+
 const controller ={
-    index: (req, res) => res.render('search-results'),
+    index: (req, res) =>{
+        db.Product.findAll({
+            where: {title:{[Op.like]:"%"+req.query.search+"%"}}
+        })
+        .then(productos =>{
+            res.render('search-results',{productos:productos})
+        })
+       
+    },
 }
 module.exports = controller;
