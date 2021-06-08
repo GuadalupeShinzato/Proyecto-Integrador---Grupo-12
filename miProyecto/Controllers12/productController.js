350946d90ec5a28f692aadc844a14df7ce883022
Guadalupe Shinzato <gshinzato@udesa.edu.ar>
2021-06-07 23:44:17 -0300

    detalles de productos y editar

--- miProyecto/Controllers12/productController.js
+++ miProyecto/Controllers12/productController.js
@@ -1,5 +1,35 @@
-const controller ={
-    index: (req, res) => res.render('product'),
+const db = require('../Database/models');
+const Op = db.Sequelize.Op;
+
+const controller = {
+    index: (req, res) => {
+        db.Product.findByPk(req.params.id, {
+            include: {
+                association: "comentarios",
+                include: {
+                    association: "usuario"
+                }
+            }
+        }).then(producto => {
+            res.render('product', {
+                producto: producto
+            });
+        })
+
+    },
+
     add: (req, res) => res.render('product-add'),
+
+    create: (req, res) =>{
+        db.Product.create({
+            title: req.body.titulo,
+            author: req.body.autor,
+            cover: req.body.portada,
+            description: req.body.descripcion   
+        })
+        .then(producto=>{
+            res.redirect("/product/id/"+producto.id)
+        })
+    }
 }
 module.exports = controller;
--- miProyecto/routes/product.js
+++ miProyecto/routes/product.js
@@ -4,7 +4,8 @@ const controller = require('../Controllers12/productController')
 
 // * GET home page. * /
 
-router.get('/', controller.index);
-router.get('/add', controller.add)
+router.get('/id/:id', controller.index);
+router.get('/add', controller.add);
+router.post('/add', controller.create)
 
 module.exports = router
